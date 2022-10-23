4b59f8c116ff351f90d48bf15d03868e5fd2e2a4
Gabriel Rocha <rochafrgabriel@gmail.com>
2022-10-22 22:23:37 -0300

    feat: Request type in req at auth middleware

--- src/middlewares/auth/index.ts
+++ src/middlewares/auth/index.ts
@@ -1,9 +1,11 @@
 import { Request, Response, NextFunction } from "express"
 import jwt from "jsonwebtoken"
 
+import { RequestWithUser } from "../../interfaces/RequestWithUser";
+
 const config = process.env
 
-const verifyToken = (req: any, res: Response, next: NextFunction) => {
+const verifyToken = (req: Request, res: Response, next: NextFunction) => {
   const token =
     req.body.token || req.query.token || req.headers["x-access-token"];
 
@@ -12,7 +14,7 @@ const verifyToken = (req: any, res: Response, next: NextFunction) => {
   }
   try {
     const decoded = jwt.verify(token, config.TOKEN_KEY as string);
-    req.user = decoded;
+    req.user = decoded as RequestWithUser;
   } catch (err) {
     return res.status(401).send("Invalid Token");
   }
